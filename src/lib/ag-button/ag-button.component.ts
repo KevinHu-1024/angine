eac492c8b4b84389511165c43b795e5f407285be
KevinHu-1024 <hi-bai-du@163.com>
2017-08-17 08:17:29 +0800

    feat: ag-button click效果

--- src/lib/ag-button/ag-button.component.ts
+++ src/lib/ag-button/ag-button.component.ts
@@ -25,19 +25,30 @@ export class AgButtonComponent implements OnInit {
     this.setClassNames();
   }
 
-  private setClassNames() {
-    this.classNames = {
-      [classPrefix]: true,
-      [`${classPrefix}-default`]: this.type === 'default' || !this.type,
-      [`${classPrefix}-primary`]: this.type === 'primary',
-      [`${classPrefix}-danger`]: this.type === 'danger',
-      [`${classPrefix}-sm`]: this.size === 'small',
-      [`${classPrefix}-lg`]: this.size === 'large',
-      [`${classPrefix}-dashed`]: this.type === 'dashed',
-      [`${classPrefix}-background-ghost`]: this.ghost,
-      [`${classPrefix}-circle`]: this.shape === 'circle',
-      [`${classPrefix}-loading`]: this.loading,
-    };
+  private handleClick() {
+    const clickAttr = `${classPrefix}-clicked`;
+    this.setClassNames(clickAttr, true);
+    setTimeout(() => this.setClassNames(clickAttr, false), 300);
+  }
+
+  private setClassNames(attr?: string, val?: boolean) {
+    if (!attr) {
+      this.classNames = {
+        [classPrefix]: true,
+        [`${classPrefix}-default`]: this.type === 'default' || !this.type,
+        [`${classPrefix}-primary`]: this.type === 'primary',
+        [`${classPrefix}-danger`]: this.type === 'danger',
+        [`${classPrefix}-sm`]: this.size === 'small',
+        [`${classPrefix}-lg`]: this.size === 'large',
+        [`${classPrefix}-dashed`]: this.type === 'dashed',
+        [`${classPrefix}-background-ghost`]: this.ghost,
+        [`${classPrefix}-circle`]: this.shape === 'circle',
+        [`${classPrefix}-loading`]: this.loading,
+        [`${classPrefix}-clicked`]: false,
+      };
+    } else {
+      this.classNames[attr] = val;
+    }
   }
 
 }
